766fb66e964b956ab8019ab597267bbad31164f7
Oportea <oporteadev@gmail.com>
2025-08-03 17:30:22 +0200

    commit

--- App.py
+++ App.py
@@ -1,10 +1,11 @@
 from flask import Flask, redirect, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def HomePage():
-    return render_template("/HomePage/Index.html")
+    return render_template("HomePage.html")
 
 if __name__ == "__main__" :
     app.run(debug=True)
