e62ddff58fb80c98965732c8cdbc11f27348f369
Oportea <oporteadev@gmail.com>
2025-08-03 16:14:18 +0200

    commit

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def HomePage():
-    return render_template("HomePage/Index.html")
+    return render_template("/HomePage/Index.html")
 
 if __name__ == "__main__" :
     app.run(debug=True)
