117b943a488136f02d936cf2d2b642a6e0425d11
Oportea <oporteadev@gmail.com>
2025-08-05 01:59:52 +0200

    commit

--- App.py
+++ App.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def HomePage():
     x = requests.get('https://api.lanyard.rest/v1/users/1359617287690391724')
-    return f"<h1>{x.reason}</h1>"
+    return f"<h1>{x.status_code}</h1>"
 
     #return render_template("HomePage.html")
 
