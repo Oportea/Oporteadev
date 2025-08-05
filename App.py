6586d43bde21c06b6acb34aa3f5872838e4a9bb1
Oportea <oporteadev@gmail.com>
2025-08-05 02:30:52 +0200

    commit

--- App.py
+++ App.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, jsonify
 import requests
+import codecs
 
 app = Flask(__name__)
 
 @app.route("/")
 def HomePage():
     Request = requests.get('https://api.lanyard.rest/v1/users/1359617287690391724')
-    Content = Request.content.replace("b", '', 1)
-    Content = Content.replace("'", '')
-    return f"<h1>{Content}</h1>"
+    return f"<h1>{codecs.decode(Request.content)}</h1>"
 
     #return render_template("HomePage.html")
 
