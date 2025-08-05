0196dec48f6a4e1579331a7f827b0ad44e0885a4
Oportea <oporteadev@gmail.com>
2025-08-05 02:21:52 +0200

    commit

--- App.py
+++ App.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import requests
+import struct
 
 app = Flask(__name__)
 
 @app.route("/")
 def HomePage():
     x = requests.get('https://api.lanyard.rest/v1/users/1359617287690391724')
-    return f"<h1>{x.content}</h1>"
+    
+    return f"<h1>{struct.unpack('>d', x.content)[0]}</h1>"
 
     #return render_template("HomePage.html")
 
