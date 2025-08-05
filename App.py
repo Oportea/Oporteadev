edf5e62beeeabfcf825e9e887a0b221025dbf691
Oportea <oporteadev@gmail.com>
2025-08-05 02:47:54 +0200

    commit

--- App.py
+++ App.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def HomePage():
     Request = requests.get('https://api.lanyard.rest/v1/users/1359617287690391724')
     Content = codecs.decode(Request.content)
-    return Content
+    return Content["data"]["discord_status"]
 
     #return render_template("HomePage.html")
 
